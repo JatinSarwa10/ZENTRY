31930f98054db72592ed057869a7cd58aae3d26f
JatinSarwa10 <sarwajatin@gmail.com>
2025-08-17 17:44:59 +0530

    Updated App.jsx, package files and added Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './components/Hero'
 import About from './components/About'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <main className='relative min-h-screen w-screen overflox-x-hidden'>
+    <main className='relative min-h-screen w-screen overflox-x-hidden bg-zinc-600'>
+    <Navbar />
     <Hero />
     <About />
     </main>
